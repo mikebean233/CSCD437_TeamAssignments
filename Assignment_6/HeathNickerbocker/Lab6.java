c9b6bfbc47d2518f96ff398bcfb2d8abb11fe414
hknickerbocker <hknickerbocker@eagles.ewu.edu>
2016-11-03 08:33:18 -0700

    java stubout

---
+++ Assignment_6/HeathNickerbocker/Lab6.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class Lab6
+{   
+   private class regexVerifier
+   {
+      public String regex;
+      public int shouldMatch;
+      public String failDescription;
+      
+      public regexVerifier(String regex, int shouldMatch, String failDescription)
+      {
+         this.regex = regex;
+         this.shouldMatch = shouldMatch;
+         this.failDescription = failDescription;
+      }
+   }
+   
+   public static void main(String[] args)
+   {
+         
+   }
+   
+   public int runRegexTestCases(String regex, String[] testCases)
+   {
+      return 0;
+   }
+   
+   public int isRegexMatch(final String regex, final String input)
+   {
+      return 0;
+   }
+   
+   public void getValidatedString(String prompt, String inputBuffer, int inputBufferSize, regexVerifier[] verifiers)
+   {
+      
+   }
+}
